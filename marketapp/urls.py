7ac6d4580890e50b8fbe3f91b70d457830a81d5c
unknown <atolagbefolakerach@gmail.com>
2025-05-01 06:19:16 +0100

    added pages

--- marketapp/urls.py
+++ marketapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.market, name="ecommrce market"),
+    path('', views.market, name="market"),
     path('checkout/', views.checkout, name="payment page"),
     path('cart/', views.cart, name="cart page")
 ]
